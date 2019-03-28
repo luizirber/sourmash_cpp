b7267fb923246e63c066b939b8a65ea994d72566
Luiz Irber <luiz.irber@gmail.com>
2019-03-28 23:19:41 +0000

    working

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 int main() {
+  KmerMinHash mh(2, 2, false, 42, 0);
+  KmerMinAbundance ma(2, 2, false, 42, 0);
+
+  mh.add_sequence("AAGTTGCA");
+  ma.add_sequence("AAAAAAAA");
+  cout << "Sequence added" << endl;
+
+  cout << "MinHash size: " << mh.size() << endl;
+  cout << "MinAbundance size: " << ma.size() << endl;
+
+  cout << "in common (mh/mh): " << mh.count_common(mh) << endl;
+  cout << "in common (mh/ma): " << mh.count_common(ma) << endl;
 
   return 0;
 }
