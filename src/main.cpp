61b856410a954d0f214a048174073ae6941257fd
Luiz Irber <luiz.irber@gmail.com>
2019-03-29 20:01:50 +0000

    clean up cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,11 @@ int main() {
   KmerMinAbundance ma(2, 2, false, 42, 0);
 
   mh.add_sequence("AAGTTGCA");
-  ma.add_sequence("AAAAAAAA");
-  cout << "Sequence added" << endl;
-
+  cout << "mh: Sequence added " << "AAGTTGCA" << endl;
   cout << "MinHash size: " << mh.size() << endl;
+
+  ma.add_sequence("AAAAAAAA");
+  cout << "ma: Sequence added " << "AAAAAAAA" << endl;
   cout << "MinAbundance size: " << ma.size() << endl;
 
   cout << "in common (mh/mh): " << mh.count_common(mh) << endl;
